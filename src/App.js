abe612b12e123288e2f3294e6328cbf63818278c
bmarci888 <bravikmarci@gmail.com>
2022-11-23 10:21:00 +0100

    donation section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { ContactForm } from './components/contact';
 import WelcomeSection from './components/welcome';
 import About from './components/about';
+import Donation from './components/donation';
 
 function App() {
   return (
     <div className="App">
       <WelcomeSection />
       <About />
+      <Donation />
       <ContactForm />
     </div>
   );
