2d853d41f2740b6e48f6cc68325902282ecc97b3
Peterfug <fugedi.peter5@gmail.com>
2022-11-24 13:40:41 +0100

    contact done

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "./contact.css";
+import contactPicture2 from "../assets/contact_2.png";
+import contactPicture1 from "../assets/contact_1.png";
+import contactPicture3 from "../assets/contact_3.png";
+import contactPicture4 from "../assets/contact_4.png";
 
 export class ContactForm extends React.Component {
   constructor(props) {
@@ -9,7 +14,7 @@ export class ContactForm extends React.Component {
       Email: "",
       Subject: "",
       YourMessage: "",
-      submitted : false,
+      submitted: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -41,7 +46,7 @@ export class ContactForm extends React.Component {
 
     if (event.target.id === "Email") {
       this.setState({
-        FirstName:this.state.FirstName,
+        FirstName: this.state.FirstName,
         LastName: this.state.LastName,
         Email: event.target.value,
         Subject: this.state.Subject,
@@ -52,7 +57,7 @@ export class ContactForm extends React.Component {
 
     if (event.target.id === "Subject") {
       this.setState({
-        FirstName:this.state.FirstName,
+        FirstName: this.state.FirstName,
         LastName: this.state.LastName,
         Email: this.state.Email,
         Subject: event.target.value,
@@ -71,7 +76,6 @@ export class ContactForm extends React.Component {
         submitted: false,
       });
     }
-  
   }
 
   handleSubmit(event) {
@@ -82,7 +86,7 @@ export class ContactForm extends React.Component {
       Email: this.state.Email,
       Subject: this.state.Subject,
       YourMessage: event.target.value,
-      submitted: true
+      submitted: true,
     });
     console.log(
       `FirstName: ${this.state.FirstName} LastName: ${this.state.LastName} Email: ${this.state.Email} Subject: ${this.state.Subject} YourMessage: ${this.state.YourMessage}`
@@ -90,64 +94,122 @@ export class ContactForm extends React.Component {
   }
 
   render() {
-    return this.state.submitted ? (<label>We appreciate you contacting us. One of our colleagues will get back in touch with you soon!
-        Have a great day!</label>): 
-    (
-      <form onSubmit={this.handleSubmit}>
-        <label>Contact Us!</label>
-        <br />
-        <label>
-          First Name:
-          <input
-            id="FirstName"
-            type="text"
-            value={this.state.FirstName}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Last Name:
-          <input
-            id="LastName"
-            type="text"
-            value={this.state.LastName}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            id="Email"
-            type="text"
-            value={this.state.Email}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Subject:
-          <input
-            id="Subject"
-            type="text"
-            value={this.state.Subject}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Your Message:
-          <input
-            id="YourMessage"
-            type="text"
-            value={this.state.YourMessage}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+    return  (
+      <div>
+        <div>
+        {this.state.submitted ? (<p className="thankY"><strong>
+        We appreciate you contacting us. One of our colleagues will get back in
+        touch with you soon! Have a great day! 
+        </strong>
+      </p>
+    ) : (
+        <form onSubmit={this.handleSubmit}>
+          <h1 className="contactHr"> Contact Us!</h1>
+          <div className="contactContainer">
+            <img alt="contact 1" className="cImage" src={contactPicture2} />
+
+            <div className="cardSubmit">
+              <label>
+                First Name:<br />
+                <input
+                  className="allInp"
+                  id="FirstName"
+                  type="text"
+                  value={this.state.FirstName}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                Last Name:<br />
+                <input
+                  className="allInp"
+                  id="LastName"
+                  type="text"
+                  value={this.state.LastName}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                Email:<br />
+                <input
+                  className="allInp"
+                  id="Email"
+                  type="text"
+                  value={this.state.Email}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                Subject:<br />
+                <input
+                  className="allInp"
+                  id="Subject"
+                  type="text"
+                  value={this.state.Subject}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                Your Message:<br />
+                <input
+                  className="allInp"
+                  id="YourMessage"
+                  type="text"
+                  value={this.state.YourMessage}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <input className="button" type="submit" value="Submit" />
+            </div>
+          </div>
+        </form>
+    )}
+        
+        </div>
+        <div className="contactAdd">
+          <div>
+            <p>
+              <strong>GREEN BELT MOVEMENT HEADQUARTERS - KENYA</strong>
+            </p>
+            <p>Adams Arcade, Kilimani Road off Elgeyo Marakwet Rd</p>
+            <p>P.O BOX 67545-00200</p>
+            <p>Nairobi, Kenya</p>
+            <p>Phone: +254 (20) 3873057 / (20) 3860158 / (20) 3860157</p>
+            <p>Phone: +254 (0) 721342696</p>
+            <p>Email: gbm@greenbeltmovement.org</p>
+          </div>
+          <img alt="contact 2" className="cImage2" src={contactPicture1} />
+        </div>
+        <div className="contactAdd2">
+          <div className="contactText">
+            <p>
+              <strong>COURIER ADDRESS (FOR DHL, FEDEX, UPS, ETC.):</strong>
+            </p>
+            <p>Kililmani Road, off Elgeyo Marakwet Road</p>
+            <p>Adams Arcade</p>
+            <p>Nairobi, Kenya</p>
+          </div>
+          <img alt="contact 3" className="cImage3" src={contactPicture3} />
+        </div>
+        <div className="contactAdd3">
+          <div>
+            <p>
+              <strong>THE GREEN BELT MOVEMENT - US OFFICE</strong>
+            </p>
+            <p>Green Belt Movement International</p>
+            <p>442 Lorimer St.</p>
+            <p>Ste D PMB 5015</p>
+            <p>Brooklyn, NY 11206</p>
+            <p>Email: gbmius@greenbeltmovement.org</p>
+          </div>
+          <img alt="contact 4" className="cImage4" src={contactPicture4} />
+        </div>
+      </div>
     );
   }
 }
